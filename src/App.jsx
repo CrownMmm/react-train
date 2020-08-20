ea73802b6aea4746e381c5b9015ea06658b76cef
zhangjiaming <823600537@qq.com>
2020-08-21 01:09:20 +0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import React, { useState, Component, useMemo, memo, useCallback } from "react";
+import React, {
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+} from "react";
 import "./App.css";
 
-const Counter = memo(function Counter(props) {
-  console.log("Counter render");
-  return <h1 onClick={props.onClick}>{props.count}</h1>;
-});
+// const Counter = memo(function Counter(props) {
+//   console.log("Counter render");
+//   return <h1 onClick={props.onClick}>{props.count}</h1>;
+// });
 
-function App(props) {
-  const [count, setCount] = useState(0);
-  const [clickCount, setClickCount] = useState(0);
+function useCounter(count) {
+  const size=useSize()  
+return <h1>{count},{size.width}x{size.height}</h1>;
+}
 
-  const double = useMemo(() => {
-    return count * 2;
-  }, [count === 3]);
+function useSize() {
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+  });
 
-  const onClick = useCallback(() => {
-    console.log("Click");
-    setClickCount((clickCount) => clickCount + 1);
+  const onResize = useCallback(() => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    });
+  }, []);
+  useEffect(() => {
+    window.addEventListener("resize", onResize, false);
+    return () => {
+      window.removeEventListener("resize", onResize, false);
+    };
+  }, []);
+  return size;
+}
+
+function useCount(defaultCount) {
+  const [count, setCount] = useState(defaultCount);
+  const it = useRef();
+  useEffect(() => {
+    it.current = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
   }, []);
 
-  //useMemo(()=> fn) 返回一个函数
-  //useCallback(fn)  等同于上面
+  useEffect(() => {
+    if (count >= 10) {
+      clearInterval(it.current);
+    }
+  });
+
+  return [count, setCount];
+}
+
+function App(props) {
+  const size = useSize();
+
+  const [count, setCount] = useCount(0);
+  const counter = useCounter(count);
   return (
     <div>
       <button
@@ -29,9 +68,9 @@ function App(props) {
           setCount(count + 1);
         }}
       >
-        double:({double}), count:({count})
+        count:({count}),{size.width}x{size.height}
       </button>
-      <Counter count={double} onClick={onClick} />
+      {counter}
     </div>
   );
 }
