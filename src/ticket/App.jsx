4bacd0a0de14622994b954ff0ec9c48c85f77ebd
zhangjiaming <823600537@qq.com>
2020-10-14 00:39:23 +0800

    feat:车次列表请求

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,18 +1,32 @@
 import { connect } from "react-redux";
-import React, {
-  useEffect,
-  useCallback,
-  useMemo,
-  lazy,
-  Suspense,
-} from 'react';
-import Detail from '../common/Detail.jsx'
-import Header from '../common/Header.jsx'
-import Nav from '../common/Nav.jsx'
-import Candidate from './Candidate.jsx'
-import Schedule from './Schedule.jsx'
+import React, { useEffect, useCallback, useMemo, lazy, Suspense } from "react";
+import Detail from "../common/Detail.jsx";
+import Header from "../common/Header.jsx";
+import Nav from "../common/Nav.jsx";
+import Candidate from "./Candidate.jsx";
+import Schedule from "./Schedule.jsx";
+import URI from "urijs";
+import dayjs from "dayjs";
+import { h0 } from "../common/fp";
+import useNav from "../common/useNav";
 
 import "./App.css";
+
+import {
+  setDepartStation,
+  setArriveStation,
+  setTrainNumber,
+  setDepartDate,
+  setSearchParsed,
+  prevDate,
+  nextDate,
+  setDepartTimeStr,
+  setArriveTimeStr,
+  setArriveDate,
+  setDurationStr,
+  setTickets,
+  toggleIsScheduleVisible,
+} from "./actions";
 
 function App(props) {
   const {
@@ -29,9 +43,91 @@ function App(props) {
     searchParsed,
     dispatch,
   } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const { aStation, dStation, date, trainNumber } = queries;
+
+    dispatch(setDepartStation(dStation));
+    dispatch(setArriveStation(aStation));
+    dispatch(setTrainNumber(trainNumber));
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+
+    dispatch(setSearchParsed(true));
+  }, []);
+
+  useEffect(() => {
+    document.title = trainNumber;
+  }, [trainNumber]);
+
+  useEffect(()=> {
+    if(!searchParsed) {
+      return;
+    }
+
+    const url = new URI('/rest/ticket')
+      .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+      .setSearch('trainNumber', trainNumber)
+      .toString();
+
+      fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        // console.log(result);
+        const {
+          detail,
+          candidates,
+        } = result;
+
+        const {
+          departTimeStr,
+          arriveTimeStr,
+          arriveDate,
+          durationStr,
+        } = detail;
+
+
+        dispatch(setDepartTimeStr(departTimeStr));
+        dispatch(setArriveTimeStr(arriveTimeStr));
+        dispatch(setArriveDate(arriveDate));
+        dispatch(setDurationStr(durationStr));
+        dispatch(setTickets(candidates));
+
+      });
+
+  },[searchParsed, departDate, trainNumber])
+
+  const { isPrevDisabled, isNextDisabled, prev, next } = useNav(
+    departDate,
+    dispatch,
+    prevDate,
+    nextDate
+  );
+
+  if (!searchParsed) {
+    return null;
+  }
+
   return (
-    <div className="app"></div>
-  )
+    <div className="app">
+      <div className="header-wrapper">
+        <Header title={trainNumber} onBack={onBack} />
+      </div>
+      <div className="nav-wrapper">
+        <Nav
+          date={departDate}
+          isPrevDisabled={isPrevDisabled}
+          isNextDisabled={isNextDisabled}
+          prev={prev}
+          next={next}
+        />
+      </div>
+    </div>
+  );
 }
 export default connect(
   function mapStateToProps(state) {
