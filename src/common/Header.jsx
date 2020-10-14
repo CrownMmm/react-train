015c6bfc6e0171525de10837a65a8098cdcee4d4
zhangjiaming <823600537@qq.com>
2020-10-15 02:33:18 +0800

    修改代码格式

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './Header.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "./Header.css";
 export default function Header(props) {
-  const  {
-    onBack,
-    title
-  } =props
+  const { onBack, title } = props;
   return (
     <div className="header">
       <div className="header-back" onClick={onBack}>
         <svg width="42" height="42">
-          <polyline 
+          <polyline
             points="25,13 16,21 25,29"
             stroke="#fff"
             strokeWidth="2"
@@ -18,14 +15,12 @@ export default function Header(props) {
           />
         </svg>
       </div>
-      <h1 className="header-title">
-        {title }
-      </h1>
+      <h1 className="header-title">{title}</h1>
     </div>
-  )
+  );
 }
 
 Header.propTypes = {
-  onBack:PropTypes.func.isRequired,
-  title:PropTypes.string.isRequired
-}
+  onBack: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+};
