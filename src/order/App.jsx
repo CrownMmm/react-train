ac508ec882338d78b74a176c9095f975884dc2ed
zhangjiaming <823600537@qq.com>
2020-10-16 00:46:29 +0800

    feat:车站详情页面

--- src/order/App.jsx
+++ src/order/App.jsx
@@ -1,20 +1,16 @@
 import { connect } from "react-redux";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-} from 'react';
-import URI from 'urijs';
-import dayjs from 'dayjs';
-import {bindActionCreators} from 'redux';
-import Header from '../common/Header.jsx';
-import Detail from '../common/Detail.jsx';
-import Account from './Account.jsx'
-import Choose from './Choose.jsx'
-import Passengers from './Passengers.jsx'
-import Ticket from './Ticket.jsx'
+import React, { useCallback, useEffect, useMemo } from "react";
+import URI from "urijs";
+import dayjs from "dayjs";
+import { bindActionCreators } from "redux";
+import Header from "../common/Header.jsx";
+import Detail from "../common/Detail.jsx";
+import Account from "./Account.jsx";
+import Choose from "./Choose.jsx";
+import Passengers from "./Passengers.jsx";
+import Ticket from "./Ticket.jsx";
 // import Menu from './Menu.jsx'
-import './App.css';
+import "./App.css";
 import {
   setDepartStation,
   setArriveStation,
@@ -31,7 +27,7 @@ import {
   showGenderMenu,
   showFollowAdultMenu,
   showTicketTypeMenu,
-} from './actions';
+} from "./actions";
 import "./App.css";
 
 function App(props) {
@@ -52,9 +48,60 @@ function App(props) {
     searchParsed,
     dispatch,
   } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+
+    const { trainNumber, dStation, aStation, type, date } = queries;
+
+    dispatch(setDepartStation(dStation));
+    dispatch(setArriveStation(aStation));
+    dispatch(setTrainNumber(trainNumber));
+    dispatch(setSeatType(type));
+    dispatch(setDepartDate(dayjs(date).valueOf()));
+    dispatch(setSearchParsed(true));
+  }, []);
+
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+
+    const url = new URI("/rest/order")
+      .setSearch("dStation", departStation)
+      .setSearch("aStation", arriveStation)
+      .setSearch("type", seatType)
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .toString();
+
+    dispatch(fetchInitial(url));
+  }, [departStation, arriveStation, seatType, departDate, searchParsed]);
+
   return (
-    <div className="app"></div>
-  )
+    <div className="app">
+      <div className="header-wrapper">
+        <Header title="订单填写" onBack={onBack}></Header>
+      </div>
+      <div className="detail-wrapper">
+        <Detail
+          departDate={departDate}
+          arriveDate={arriveDate}
+          departTimeStr={departTimeStr}
+          arriveTimeStr={arriveTimeStr}
+          trainNumber={trainNumber}
+          departStation={departStation}
+          arriveStation={arriveStation}
+          durationStr={durationStr}
+        >
+          <span style={{display: 'block'}} className="train-icon"></span>
+        </Detail>
+      </div>
+    </div>
+  );
 }
 export default connect(
   function mapStateToProps(state) {
