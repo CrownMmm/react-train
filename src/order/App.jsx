bf293dafa749855a5f83830cd3be929a1e8a4dc6
zhangjiaming <823600537@qq.com>
2020-10-17 01:41:49 +0800

    feat:座位填写

--- src/order/App.jsx
+++ src/order/App.jsx
@@ -138,6 +138,13 @@ function App(props) {
       </div>
       <Ticket price={price} type={seatType} />
       <Passengers passengers={passengers} {...passengersCbs} />
+      { passengers.length > 0 && (
+          <Choose
+            passengers={passengers}   
+            {...chooseCbs}         
+          />
+        )
+      }
       <Menu show={isMenuVisible} {...menu} {...menuCbs} />
     </div>
   );
--- src/order/Choose.jsx
+++ src/order/Choose.jsx
@@ -1,20 +1,57 @@
-import React, {
-	memo,
-} from 'react';
-import classnames from 'classnames';
-import PropTypes from 'prop-types';
-import './Choose.css';
+import React, { memo } from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import "./Choose.css";
 
-const Choose = memo(function Choose() {
+const Choose = memo(function Choose(props) {
+  const { passengers, updatePassenger } = props;
+
+  function createSeat(seatType) {
+    return (
+      <div>
+        {passengers.map((passenger) => {
+          return (
+            <p
+              key={passenger.id}
+              className={classnames("seat", {
+                active: passenger.seat === seatType,
+              })}
+              data-text={seatType}
+              onClick={() =>
+                updatePassenger(passenger.id, {
+                  seat: seatType,
+                })
+              }
+            >
+              &#xe02d;
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="choose">
-      <div>1</div>
+      <p className="tip">在线选座</p>
+      <div className="container">
+        <div className="seats">
+          <div className="container">窗</div>
+          {createSeat("A")}
+          {createSeat("B")}
+          {createSeat("C")}
+          <div className="container">过道</div>
+          {createSeat("D")}
+          {createSeat("F")}
+          <div className="container">窗</div>
+        </div>
+      </div>
     </div>
   );
 });
 export default Choose;
 
 Choose.propTypes = {
-	passengers: PropTypes.array.isRequired,
-	updatePassenger: PropTypes.func.isRequired,
-}
+  passengers: PropTypes.array.isRequired,
+  updatePassenger: PropTypes.func.isRequired,
+};
